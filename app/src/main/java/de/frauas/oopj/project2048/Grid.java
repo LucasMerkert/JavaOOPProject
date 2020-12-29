5b9437f50e87297446d7b9cb1487fe5506c999ac
Tarik Hammad <tarik.hammad@stud.fra-uas.de>
2020-12-29 12:46:37 +0100

    Added Grid, Tile, and TileSpawnException classes

---
+++ app/src/main/java/de/frauas/oopj/project2048/Grid.java
@@ -0,0 +1,36 @@
+package de.frauas.oopj.project2048;
+
+/**
+ * @author Tarik
+ *
+ */
+public class Grid {
+    Tile[][] matrix;
+    int width;
+    int height;
+
+    /**
+     * Contructor for Grid object, the main playing field. The top-left corner of the grid is [0][0], the one to the right of it [1][0]; the one below [0][1]
+     * @param width
+     * @param height
+     */
+    public Grid(int width, int height) {
+        this.width = width;
+        this.height = height;
+        matrix = new Tile[width][height];
+    }
+
+    void swipeUp() {
+        for(int column=0; column<width; column++) {
+            for(int row=0; row<height; row++) {
+                if(matrix[column][row] != null) {
+
+                }
+            }
+        }
+    }
+
+
+
+
+}
---
+++ app/src/main/java/de/frauas/oopj/project2048/Tile.java
@@ -0,0 +1,30 @@
+package de.frauas.oopj.project2048;
+
+/**
+ * @author Tarik
+ *
+ */
+public class Tile {
+    private int value;
+
+    /**
+     * Constructor for tile object,
+     * @param value
+     * @throws TileSpawnException if value is not 2 or 4
+     */
+    public Tile(int value) throws TileSpawnException{
+        if (value == 2 || value == 4) {
+            this.value=value;
+        } else throw new TileSpawnException();
+    }
+    /**
+     * Levels up (doubles in value) the corresponding tile
+     */
+    public void levelUP() {
+        value = 2*value;
+        if (value>=2048) {
+            //TODO: Winning condition 1
+        }
+    }
+
+}
