24cbfbdb0fe7a297fd6cd7a2441dc0f9ac9facc3
Friedrich Just <62269035+FriedrichJust@users.noreply.github.com>
2020-12-29 11:48:30 +0100

    Test 2 F

--- app/src/main/java/de/frauas/oopj/project2048/MainActivity.java
+++ app/src/main/java/de/frauas/oopj/project2048/MainActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 public class MainActivity extends AppCompatActivity {
 
     //test1
+    //F war hier
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
