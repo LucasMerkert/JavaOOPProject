3f2ce72ce77a4cbcacafe0a97748ada39279482c
Tarik Hammad <tarik.hammad@stud.fra-uas.de>
2021-01-02 14:32:28 +0100

    New Background Images

--- app/src/main/java/de/frauas/oopj/project2048/Grid4x4.java
+++ app/src/main/java/de/frauas/oopj/project2048/Grid4x4.java
@@ -20,29 +20,19 @@ public class Grid4x4 extends AppCompatActivity {
 
         HEIGHT = getIntent().getIntExtra("EXTRA_GRID_HEIGHT", 0);
         WIDTH = getIntent().getIntExtra("EXTRA_GRID_WIDTH", 0);
+
         TextView titleText = findViewById(R.id.InGameTitleString);
         titleText.setText(String.format("This is a %dx%d game.", HEIGHT, WIDTH));
 
-    /*
-        // Create a ConstraintLayout in which to add the ImageView
-        ConstraintLayout constraintLayout = findViewById(R.id.activity_grid_layout);
-
         // Instantiate an ImageView and define its properties
         ImageView background_image = findViewById(R.id.gameGrid);
-        background_image.setImageResource(R.drawable.background4x4);
-        //background_image.setContentDescription(getResources().getString(R.string.));
+        if(WIDTH == 4 && HEIGHT == 4) background_image.setImageResource(R.drawable.grid4x4);
+        else if (WIDTH == 5 && HEIGHT == 5) background_image.setImageResource(R.drawable.grid5x5);
+        else if (WIDTH == 6 && HEIGHT == 6) background_image.setImageResource(R.drawable.grid6x6);
 
-        // set the ImageView bounds to match the Drawable's dimensions
-        background_image.setAdjustViewBounds(true);
-        background_image.setLayoutParams(new ViewGroup.LayoutParams(
-                ViewGroup.LayoutParams.WRAP_CONTENT,
-                ViewGroup.LayoutParams.WRAP_CONTENT));
-
-        // Add the ImageView to the layout and set the layout as the content view.
-
-        constraintLayout.addView(background_image);
-        setContentView(constraintLayout);*/
     }
+
+
 
 
 }
