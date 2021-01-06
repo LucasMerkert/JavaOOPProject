a52fe619292ca36f427f7d95b319fb65d973600d
LucasMerkert <lucas.merkert@gmx.net>
2021-01-06 13:45:28 +0100

    gradle wrapper update

--- app/src/main/java/de/frauas/oopj/project2048/Grid4x4.java
+++ app/src/main/java/de/frauas/oopj/project2048/Grid4x4.java
@@ -26,7 +26,7 @@ public class Grid4x4 extends AppCompatActivity {
 
         // Instantiate an ImageView and define its properties
         ImageView background_image = findViewById(R.id.gameGrid);
-        if (WIDTH == 4 && HEIGHT == 4) background_image.setImageResource(R.drawable.grid4x4);
+        if (WIDTH == 4 && HEIGHT == 4) background_image.setImageResource(R.drawable.grid4x4_background);
         else if (WIDTH == 5 && HEIGHT == 5) background_image.setImageResource(R.drawable.grid5x5);
         else if (WIDTH == 6 && HEIGHT == 6) background_image.setImageResource(R.drawable.grid6x6);
 
